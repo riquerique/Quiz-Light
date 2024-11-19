67b8d0f10172de57f8b15c37ae653dc01d8a76b2
Gabriel Freitas dos Santos <gabrielfreitasriorj@gmail.com>
2024-11-19 15:31:46 -0300

    Icones da tela final
    Icones da tela final

--- scripts/resultado.gd
+++ scripts/resultado.gd
@@ -6,14 +6,18 @@ func _ready() -> void:
 		$Profile.text = "Família Responsável"
 		$Description.text = "Parabéns! Vocês têm hábitos exemplares de consumo consciente de água. Continuem assim e inspire
 outras pessoas!"
+		$Icon.texture = load('res://Assets/Questions/Light/Images/3.png')
+		
 	elif GlobalVar.pontos >= 45:
 		$Profile.text = "Família Atenta"
 		$Description.text = "Vocês estão no caminho certo, mas ainda pode melhorar. Pequenas mudanças podem fazer uma grande
 diferença no consumo!"
+		$Icon.texture = load('res://Assets/Questions/Light/Images/2.png')
 	else:
 		$Profile.text = "Família Distraída"
 		$Description.text = "Há espaço para melhorias nos seus hábitos de consumo de água. Que tal começar adotando algumas
 práticas simples?"
+		$Icon.texture = load('res://Assets/Questions/Light/Images/1.png')
 
 func _on_button_pressed() -> void:
 	GlobalVar.pontos = 0
