d1940b789c11e9b8b1e790a9b2cce0bab6d309a5
Rique <riquerique210105@gmail.com>
2024-12-08 19:59:05 -0300

    Tela de Resultado
    Troca de fonte e conserto na quebra de linha

--- scripts/resultado.gd
+++ scripts/resultado.gd
@@ -4,19 +4,16 @@ extends Control
 func _ready() -> void:
 	if GlobalVar.pontos >= 75:
 		$Profile.text = "Família Responsável"
-		$Description.text = "Parabéns! Vocês têm hábitos exemplares de consumo consciente de água. Continuem assim e inspire
-outras pessoas!"
+		$Description.text = "Parabéns! Vocês têm hábitos exemplares de consumo consciente de água. Continuem assim e inspire outras pessoas!"
 		$Icon.texture = load('res://Assets/Questions/Light/Images/3.png')
 		
 	elif GlobalVar.pontos >= 45:
 		$Profile.text = "Família Atenta"
-		$Description.text = "Vocês estão no caminho certo, mas ainda pode melhorar. Pequenas mudanças podem fazer uma grande
-diferença no consumo!"
+		$Description.text = "Vocês estão no caminho certo, mas ainda pode melhorar. Pequenas mudanças podem fazer uma grande diferença no consumo!"
 		$Icon.texture = load('res://Assets/Questions/Light/Images/2.png')
 	else:
 		$Profile.text = "Família Distraída"
-		$Description.text = "Há espaço para melhorias nos seus hábitos de consumo de água. Que tal começar adotando algumas
-práticas simples?"
+		$Description.text = "Há espaço para melhorias nos seus hábitos de consumo de água. Que tal começar adotando algumas práticas simples?"
 		$Icon.texture = load('res://Assets/Questions/Light/Images/1.png')
 
 func _on_button_pressed() -> void:
